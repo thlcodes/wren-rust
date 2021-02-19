c82e6d784f12e9e3be421b0964abe5aaeb8985cd
Thomas Liebeskind <thomas.liebeskind@audi.de>
2021-02-19 12:49:37 +0100

    adapt new makefile structure

--- wren-sys/build.rs
+++ wren-sys/build.rs
@@ -3,15 +3,15 @@ use std::path::Path;
 use std::process::Command;
 
 #[allow(dead_code)]
-fn make_debug(dir: &Path) {
-    let status = Command::new("make").current_dir(dir).arg("debug").status();
+fn make_debug(dir: &Path, carg: &str) {
+    let status = Command::new("make").current_dir(dir).arg("-C").arg(carg).arg("config=debug_64bit").status();
     assert!(status.unwrap().success());
-    println!("cargo:rustc-link-lib=static=wrend");
+    println!("cargo:rustc-link-lib=static=wren_d");
 }
 
 #[allow(dead_code)]
-fn make_release(dir: &Path) {
-    let status = Command::new("make").current_dir(dir).arg("static").status();
+fn make_release(dir: &Path, carg: &str) {
+    let status = Command::new("make").current_dir(dir).arg(carg).arg("config=release_64bit").status();
     assert!(status.unwrap().success());
     println!("cargo:rustc-link-lib=static=wren");
 }
@@ -21,9 +21,13 @@ fn main() {
     let manifest_path = Path::new(&manifest_dir);
     let wren_make_dir = manifest_path.join("wren");
     let wren_lib_dir = manifest_path.join("wren/lib");
+    let mut carg = "projects/make/";
+    if env::consts::OS == "macos" {
+        carg = "projects/make.mac/";
+    }
 
     #[cfg(debug_assertions)]
-    make_debug(&wren_make_dir);
+    make_debug(&wren_make_dir, carg);
     #[cfg(not(debug_assertions))]
     make_release(&wren_make_dir);
 
